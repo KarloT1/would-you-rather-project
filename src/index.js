8f43b6f91a96140b278bd1e23c29f0b9ec72bcf4
KarloTomasic <karlo@northwest.agency>
2022-03-03 14:22:43 +0100

    Add middleware and initialize store in root js file.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers/index'
+import middleware from './middleware';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  middleware
+)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
